1a30cc20a7d24b7b3bd6129c908aac4e320211d9
franco <francog845@gmail.com>
2025-04-08 11:06:48 -0500

    ajustes

--- src/main/java/com/prueba/common_library/dto/UsuarioDto.java
+++ src/main/java/com/prueba/common_library/dto/UsuarioDto.java
@@ -11,20 +11,12 @@ import lombok.*;
 @AllArgsConstructor
 public class UsuarioDto {
 
-    
     private Long id;
-    @NotBlank(message = "El nombre es obligatorio")
     private String nombre;
-    @NotBlank(message = "El apellido es obligatorio")
     private String apellido;
-    @Email(message = "El correo no tiene un formato válido")
-    @NotBlank(message = "El correo es obligatorio")
     private String correo;
-    @NotBlank(message = "La contrasena es obligatoria")
     private String contrasena;
-    @NotBlank(message = "El rol es obligatorio")
     private String rol;
-    @NotBlank(message = "El user name es obligatorio")
     private String userName;
     
 }
